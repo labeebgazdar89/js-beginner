501e623bc72148ed57a6de7a11a32849dd2ffc5e
labeebgazdar89 <labeeb.sybex@gmail.com>
2024-08-21 15:08:11 +0000

    Datatypes done

---
+++ 01-basics/dataTypes.js
@@ -0,0 +1,20 @@
+'use strict' //treat all JS code as newer version
+
+let age = 12;
+let name = "Labeeb";
+let isLoggedIn = true;
+
+//primitive datatypes
+
+//int => 2 to power 53 (range of number)
+//string
+//bigInt
+//boolean 
+//null
+//undefined
+//symbol
+
+//object
+
+console.log(typeof null) //object
+console.log(typeof undefined) //undefined
