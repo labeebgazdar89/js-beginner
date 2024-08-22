8213e4baa20e8b4c160219ef356bb0282b872945
labeebgazdar89 <labeeb.sybex@gmail.com>
2024-08-22 15:40:11 +0000

    string file save

---
+++ 01-basics/strings.js
@@ -0,0 +1,14 @@
+const name = "Labeeb";
+const age = 35;
+
+console.log("Name : "+name+ " Age : "+age)
+
+console.log(`Hello my name is ${name} and my age is ${age}`);
+
+const string1 = new String("Labeeb Gazdar");
+
+console.log(typeof string1) //object
+console.log(string1[1]) //a
+console.log(string1.length) //provides length of string
+
+console.log(string1.__proto__);
