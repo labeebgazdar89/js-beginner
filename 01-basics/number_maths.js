3860f268a632ea6db9e218f9f94f22b7160b0a04
labeebgazdar89 <labeeb.sybex@gmail.com>
2024-08-23 15:37:02 +0000

    Number learning done

---
+++ 01-basics/number_maths.js
@@ -0,0 +1,11 @@
+const score = 100;
+console.log(score); //100
+
+const number1 = new Number(400);  //specifically number
+console.log(number1)
+
+const number2 = 5000000;
+console.log(number2.toFixed(2))  //provides decimal values
+console.log(number2.toPrecision(2))
+console.log(number2.toLocaleString()) //provides value in comma seperated way
+console.log(number2.toLocaleString("en-PK")); //gives value in pakistani way
