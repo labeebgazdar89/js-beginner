5c494c493126df4a98c6bd8ee226197ca0152c17
labeebgazdar89 <labeeb.sybex@gmail.com>
2024-08-27 15:36:23 +0000

    functions in process

---
+++ 01-basics/functions.js
@@ -0,0 +1,11 @@
+ function addTwoNumbers(num1, num2){
+    console.log(num1 + num2);
+    // let num3 = num1 + num2;
+    return num2 + num1;
+    console.log("Hello") //ye line print nahi hogi q k return k bd kuch print nahi hota
+}
+addTwoNumbers(3,5) //8
+addTwoNumbers(3, "5") //35
+
+const result = addTwoNumbers(8,7);
+console.log("Result : ", result) //Result :  undefined q k koi value return nahi horahi
