5f64eec08efc19cfa6b410f95b138336f2b20710
labeebgazdar89 <labeeb.sybex@gmail.com>
2024-08-29 13:19:32 +0000

    functions done

--- 01-basics/functions.js
+++ 01-basics/functions.js
@@ -9,3 +9,49 @@ addTwoNumbers(3, "5") //35
 
 const result = addTwoNumbers(8,7);
 console.log("Result : ", result) //Result :  undefined q k koi value return nahi horahi
+
+function userLoginMessage(username){
+
+    if(username===undefined){
+
+        console.log('please enter a username')
+        return
+    }
+    return `${username} just logged in`
+}
+userLoginMessage("Labeeb") //nothing will happen
+console.log(userLoginMessage("Labeeb"));
+console.log(userLoginMessage()); //undefined just logged in without argument
+
+
+//rest operator in functions '...'
+
+function calculateCartPrice(val1, val2, ...number1){
+    return number1
+}
+console.log(calculateCartPrice(200,300,400)) //[ 200, 300, 400 ]
+console.log(calculateCartPrice(200,300,400,650,470,980)) //[ 400, 650, 470, 980 ]
+
+//functions with objects
+
+const product = {
+    name : "Product1",
+    price : 199
+}
+function handleObject(anyObject){
+    console.log(`Product Name : ${anyObject.name} Price : ${anyObject.price}`)
+}
+// handleObject(product)
+
+//direct object passing
+handleObject({
+    name : "Product 2",
+    price : 299
+})
+
+//array passing in functions
+const myArray = [200,400,300,455,900];
+function returnSecondValue(getArray){
+    return getArray[1];
+}
+console.log(returnSecondValue(myArray))
