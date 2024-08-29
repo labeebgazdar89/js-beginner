9021942f3afa4d3b232f5d775a7865eddf8c2a3f
labeebgazdar89 <labeeb.sybex@gmail.com>
2024-08-29 15:13:16 +0000

    iife done

---
+++ 01-basics/iife.js
@@ -0,0 +1,19 @@
+//Immediately Invoked Function Expression
+//global variables k pollution se bachne k lye iife use karte hain
+//foran function run karne k lye iife use karte hain
+
+
+function method1(){
+    console.log(`DB Connected`)
+}
+method1();
+
+//Named IIFE
+( function method2(){
+    console.log(`DB connected 2`)
+}) (); //agr xyada functions hain to end main semi colon lagana hai
+
+//Unknown IIFE with argument 
+( (name) => {
+    console.log(`DB connected ${name}`)
+})("Labeeb");
