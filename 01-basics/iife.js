a0abc0de57eb962ffc801df850d685c99c1268d9
labeebgazdar89 <labeeb.sybex@gmail.com>
2024-08-30 13:29:55 +0000

    ifelse done

--- 01-basics/iife.js
+++ 01-basics/iife.js
@@ -17,3 +17,11 @@ method1();
 ( (name) => {
     console.log(`DB connected ${name}`)
 })("Labeeb");
+
+
+
+//Javascript Three phases
+// 1. Global Execution Phase
+// 2. Memory Creation Phase -> varibales or methods just define hote hain
+// 3. Execution Phase -> variables or functions ko values assign hoti hain
+// har function execution main ek alag se memory phase banta hai or wo execution k bd delete hpjata hai
---
+++ 02-controlflow/ifelse.js
@@ -0,0 +1,45 @@
+// const userLoggedIn = true;
+// const debitCard = true;
+
+// if(userLoggedIn && debitCard){
+//     console.log("Code Executed") //Code Executed
+// }
+// else {
+//     console.log("Code Not Executed")
+// }
+
+// const userLoggedIn = true;
+// const debitCard = false;
+
+// if(userLoggedIn && debitCard){
+//     console.log("Code Executed") 
+// }
+// else {
+//     console.log("Code Not Executed") // Code Not Executed
+// }
+
+
+if(2 == "2"){
+    console.log("true") // true
+}
+else{
+    console.log("false")
+}
+
+if(2 === "2"){
+    console.log("true") 
+}
+else{
+    console.log("false") //false
+}
+
+const userLoggedIn = true;
+const debitCard = false;
+
+if(userLoggedIn || debitCard){
+    console.log("Code Executed") //Code Executed
+}
+else {
+    console.log("Code Not Executed")
+}
+
